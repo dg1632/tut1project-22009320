47ceca6c750d50b850bbc056c53a43661e8121ea
unknown <1916708778@qq.com>
2023-09-07 16:54:07 +0800

    Added feature 1

--- Greeter.java
+++ Greeter.java
@@ -13,14 +13,14 @@ public class Greeter {
 
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-    return messages.getString("greeting") + ", " + name;
+    return messages.getString("greeting") + ", " + name.toUpperCase();
   }
 
   public static void main(String[] args) {
     String language = args[0];
     String country = args[1];
-    String name = args[2];
+    String name = args[5];
     Greeter greeter = new Greeter(language, country, name);
-   // System.out.println(greeter.sayHello());
+    System.out.println(greeter.sayHello());
   }
 }
